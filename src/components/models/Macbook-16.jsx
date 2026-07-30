023b8d1f209885453efd4fa48893558a5f930992
Daniel <daniel.olivqueiroz@outlook.com>
2026-07-30 18:29:12 -0300

    feat: add GSAP integration and new components for enhanced product visualization

--- src/components/models/Macbook-16.jsx
+++ src/components/models/Macbook-16.jsx
@@ -9,7 +9,7 @@ Title: macbook pro M3 16 inch 2024
 */
 import { useGLTF, useTexture } from "@react-three/drei";
 
-export function Model(props) {
+export default function MacbookModel16(props) {
   const { nodes, materials } = useGLTF("/models/macbook-16-transformed.glb");
 
   const texture = useTexture("/dashboard-screen.png");
---
+++ src/components/three/ModelSwitcher.jsx
@@ -0,0 +1,72 @@
+import { PresentationControls } from "@react-three/drei";
+import { useRef } from "react";
+import MacbookModel14 from "../models/Macbook-14";
+import MacbookModel16 from "../models/Macbook-16";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const ANIMATION_DURATION = 1;
+const OFFSET_DISTANCE = 5;
+
+const fadeMeshes = (group, opacity) => {
+  if (!group) return;
+
+  group.traverse((child) => {
+    if (child.isMesh) {
+      child.material.transparent = true;
+      gsap.to(child.material, { opacity, duration: ANIMATION_DURATION });
+    }
+  });
+};
+
+const moveGroup = (group, x) => {
+  if (!group) return;
+
+  gsap.to(group.position, { x, duration: ANIMATION_DURATION });
+};
+
+function ModelSwitcher({ scale, isMobile }) {
+  const smallMackbookRef = useRef();
+  const largeMackbookRef = useRef();
+
+  const showLargeMacbook = scale === 0.08 || scale === 0.05;
+
+  useGSAP(() => {
+    if (showLargeMacbook) {
+      moveGroup(smallMackbookRef.current, -OFFSET_DISTANCE);
+      moveGroup(largeMackbookRef.current, 0);
+
+      fadeMeshes(smallMackbookRef.current, 0);
+      fadeMeshes(largeMackbookRef.current, 1);
+    } else {
+      moveGroup(smallMackbookRef.current, 0);
+      moveGroup(largeMackbookRef.current, OFFSET_DISTANCE);
+
+      fadeMeshes(smallMackbookRef.current, 1);
+      fadeMeshes(largeMackbookRef.current, 0);
+    }
+  }, [scale]);
+
+  const controlsConfig = {
+    speed: 1,
+    zoom: 1,
+    config: { mass: 1, tension: 0, friction: 26 },
+  };
+
+  return (
+    <>
+      <PresentationControls {...controlsConfig}>
+        <group ref={largeMackbookRef}>
+          <MacbookModel16 scale={isMobile ? 0.05 : 0.08} />
+        </group>
+      </PresentationControls>
+      <PresentationControls {...controlsConfig}>
+        <group ref={smallMackbookRef}>
+          <MacbookModel14 scale={isMobile ? 0.03 : 0.06} />
+        </group>
+      </PresentationControls>
+    </>
+  );
+}
+
+export default ModelSwitcher;
